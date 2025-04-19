cc780c4748f2a9a1b8e8800e8a477864506baf9a
RindraIBI <rindraibi@gmail.com>
2025-04-19 22:31:41 +0200

    Project initialization

---
+++ HLA_Orchestrator/src/Application/Presentation/Controller/MainController.java
@@ -0,0 +1,9 @@
+package Application.Presentation.Controller;
+
+import javax.swing.JOptionPane;
+
+public class MainController {
+	public static void main(String []args) {
+		JOptionPane.showMessageDialog(null, "Hello DTS");
+	}
+}
